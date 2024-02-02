a1abb91bad9e8bae83ab5e842049a11e07a41c7d
Dr.Robotnik <jackj.1988@googlemail.com>
2024-02-02 12:32:47 +0000

    small tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header'
-import Home from './pages/Home'
 import Footer from './components/Footer'
+import Home from './pages/Home'
 import ProjectGallery from './pages/ProjectGallery'
 import Contact from './pages/Contact'
 
