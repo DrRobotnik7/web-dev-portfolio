d50c23f90857ff1ee3343a1bcae074afdbda6386
Dr.Robotnik <jackj.1988@googlemail.com>
2024-02-02 11:59:28 +0000

    changed form back#

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -17,7 +17,7 @@ function Contact() {
         <p>CV: <a href={CV} target="_blank">Jack Johnson Web Developer</a></p>
       </div>
     <h3>Contact Me</h3>
-    <form className="m-3" name="contact" netlify method="POST" autocomplete="off">
+    <form className="m-3" action="https://getform.io/f/13fdd2c6-8887-4bfd-87c1-025d67d56172" method="POST" autocomplete="off">
       <div className="form-group mb-3" controlId="formBasicName">
         <label for="name">Your Name</label>
       <input className="form-control" type="text" name="name" placeholder="Anne Onymous" />
